9c050587971549619811c0d0f3284aba2164b3db
Leeharim-korean <67570225+Leeharim-korean@users.noreply.github.com>
2021-07-02 22:29:18 +0900

    Make 9x9 table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import React from "react";
 import Table from "./Table";
+import "./App.css";
+
+const bigCell = [
+  {
+      id: 1,
+  },
+  {
+      id: 2,
+  },
+  {
+      id: 3,
+  },
+  {
+      id: 4,
+  },
+  {
+      id: 5,
+  },
+  {
+      id: 6,
+  },
+  {
+      id: 7,
+  },
+  {
+      id: 8,
+  },
+  {
+      id: 9,
+  }
+]
 
 function App() {
   return (
    <div>
      <h1>Hello!</h1>
+     <tbody class="big">
+        {bigCell.map(cell => (
+            <div class="small">{bigCell.map(cell =><Table id={cell.id}/>)}</div>
+             )
+        )}
+     </tbody>
+     
    </div>
   );
 }
---
+++ src/Table.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Table.css";
+
+function Table({ id }) {
+    return (
+        <td >{id}</td> 
+    );
+}
+
+export default Table;
